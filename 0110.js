ce21b1d8345e7df426a5bf86e1976fe275b869b1
KimSungJin <rainbow96bear@gmail.com>
2023-01-10 12:01:18 +0900

    카펫

---
+++ 0110.js
@@ -0,0 +1,13 @@
+function solution(brown, yellow) {
+  var answer = [];
+  for (var i = 1; i * i <= yellow; i++) {
+    if (!(yellow % i) && 2 * (yellow / i + i) + 4 == brown) {
+      answer = [yellow / i + 2, i + 2];
+    }
+  }
+  console.log(answer);
+  return answer;
+}
+solution(10, 2);
+solution(8, 1);
+solution(24, 24);
